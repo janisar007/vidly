1947278a0e0f53817f9843b8048db6b5bea4ad07
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-05-23 14:19:18 +0530

    1. since hum ab intehration test karenge jisme outter db se baat krna hoga to hum ek different vidly_test db ko banaenge using config/test.json ko bana kar. 2. created config/test.json, modification in startup/db.js, config/default.json, 3. vid 183, 4. IMP ihave set our db connection string to vidly_test using cmd ':NODE_ENV=test'. to go back to original string run ':NODE_ENV=development'

--- startup/db.js
+++ startup/db.js
@@ -1,11 +1,13 @@
 const winston = require('winston');
 const { logger } = require('../startup/logging.js');
 const mongoose = require('mongoose'); //an object
+const config = require('config');
 
 const db = function() {
     //connection to DB.
-    mongoose.connect('mongodb://127.0.0.1/vidly')
-    .then(() => logger.info('Connected to MongoDB...'));
+    const db = config.get('db'); //here db = 'mongodb://127.0.0.1/vidly'   from vid183
+    mongoose.connect(db)
+    .then(() => logger.info(`Connected to ${ db }...`));
     // .catch(err => console.error('Could not connect to MongoDB...', err)); //now we dont need this catch because isse bas hum console per log kar rahe, isse process trminate nahi ho raha hai. jab mongodb se connect nahi hoga to winston dekh lega.
 
     //note-> console.log('Connected to MongoDB...') ko winston.info('Connected to MongoDB...') me change krne se terminal per Connected to MongoDB... likh anhi aega. 
