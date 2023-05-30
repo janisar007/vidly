020ad8bb0f90ee864ccfbdf548ca88a857e9bb6c
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-05-30 12:28:19 +0530

    1. some changes for deploying on cyclic using mongodb.com

--- startup/db.js
+++ startup/db.js
@@ -6,11 +6,11 @@ const config = require('config');
 const db = function() {
     //connection to DB.
     const db = config.get('db'); //here db = 'mongodb://127.0.0.1/vidly'   from vid183
-    mongoose.connect(db, {
+    const con = mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
-    .then(() => logger.info(`Connected to ${ db }...`));
+    .then(() => logger.info(`Connected to ${ con.connection.host }...`));
     // .catch(err => console.error('Could not connect to MongoDB...', err)); //now we dont need this catch because isse bas hum console per log kar rahe, isse process trminate nahi ho raha hai. jab mongodb se connect nahi hoga to winston dekh lega.
 
     //note-> console.log('Connected to MongoDB...') ko winston.info('Connected to MongoDB...') me change krne se terminal per Connected to MongoDB... likh anhi aega. 
